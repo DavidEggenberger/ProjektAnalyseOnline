c4113fff9e4a010cbdb490520fd8e69ab1cdfd69
DavidEggenberger <david99.eggenberger@gmail.com>
2023-12-03 07:11:02 +0100

    analysis bar

--- Shared/ProjectAnalysis/ProjectAnalysisDTO.cs
+++ Shared/ProjectAnalysis/ProjectAnalysisDTO.cs
@@ -14,7 +14,7 @@ namespace Shared.ProjectAnalysis
         public DateTime CreationDate { get; set; }
         public List<AnswerDTO> Answers { get; set; }
 
-        public AnswerChoiceDTO Overall => AnswerHelper.GetOerallChoice(Answers.Select(s => ((int)s.Choice - 3)).Aggregate((s, t) => s += t));
+        public AnswerChoiceDTO Overall => AnswerHelper.GetOerallOverallChoice(Answers.Select(s => ((int)s.Choice - 3)).Aggregate((s, t) => s += t));
         public AnswerChoiceDTO Society => AnswerHelper.GetOerallChoice(Answers.Where(a => a.Question.Domain == QuestionDomainDTO.Gesellschaft).Select(s => ((int)s.Choice - 3)).Aggregate((s, t) => s += t));
         public AnswerChoiceDTO Nature => AnswerHelper.GetOerallChoice(Answers.Where(a => a.Question.Domain == QuestionDomainDTO.Umwelt).Select(s => ((int)s.Choice - 3)).Aggregate((s, t) => s += t));
         public AnswerChoiceDTO Economic => AnswerHelper.GetOerallChoice(Answers.Where(a => a.Question.Domain == QuestionDomainDTO.Wirtschaft).Select(s => ((int)s.Choice - 3)).Aggregate((s, t) => s += t));
